5ac9a2ffc66dac0c71e03639255e96ddaba58211
quockhang <quockhangtr@gmail.com>
2019-11-25 22:42:56 -0600

    mile stone 1 but not not for every file

--- cheaters.cpp
+++ cheaters.cpp
@@ -27,12 +27,14 @@ int getdir (string dir, vector<string> &files)
     return 0;
 }
 
+
 void getletters(const char *fName, vector<char>& letter, int num){
   string line;
   ifstream myfile (fName);
   vector<string> queue;
   if(myfile.is_open()){
-    while(getline(myfile, line)){
+    while(getline(myfile, line) && line != "<br><br><b>Bibliography</b><br><br>"){
+      cout << line<< endl;
         const char *letters = line.c_str();
         for(int i = 0; letters[i] != '\0'; i++){
           if('A' <= letters[i] && letters[i] <= 'Z' ){
@@ -47,10 +49,11 @@ void getletters(const char *fName, vector<char>& letter, int num){
   }
 }
 
+
 int main(int argc, char *argv[])
 {
     string dir = argv[1];
-    //const int N_WORDS = atoi(argv[2]);
+    const int N_WORDS = atoi(argv[2]);
     vector<string> files = vector<string>();
     vector<char> letters;
     vector<string> words;
@@ -61,13 +64,25 @@ int main(int argc, char *argv[])
     //     cout << i <<". " << files[i] << endl;
     // }
     string path = dir + "/" + files[0];
-    cout << path<< endl;
+    // cout << path<< endl;
     getletters(path.c_str(), letters, 6);
 
-    for (unsigned int i = 0;i < letters.size();i++) {
-        cout << letters[i];
+    // for (unsigned int i = 0;i < letters.size();i++) {
+    //     cout << letters[i];
+    // }
+    // cout << endl;
+    vector<char>::iterator it = letters.begin();
+    while (letters.size() >= N_WORDS){
+      string s;
+      for(int i = 0; i < N_WORDS; i++){
+        s = s + letters[i];
+      }
+      words.push_back(s);
+      letters.erase(letters.begin());
     }
-    cout << endl;
+    for (unsigned int i = 0; i < words.size();i++) {
+        cout << i <<". " << words[i] << endl;
+    }
 
 
     return 0;
