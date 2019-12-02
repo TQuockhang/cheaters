ea12c6f803c6187753ef479d4de3e9e99db5fda6
quockhang <quockhangtr@gmail.com>
2019-12-01 20:27:13 -0600

    made code more modular

--- cheaters.cpp
+++ cheaters.cpp
@@ -7,6 +7,27 @@
 #include <fstream>
 
 using namespace std;
+int getdir (string dir, vector<string> &files);
+void GetWords(const char *fName, vector<string>& queue);
+void convertToKey( vector<string>& queue, vector<string>& output, int num);
+
+
+
+int main(int argc, char *argv[])
+{
+    string dir = argv[1];
+    const int N_WORDS = atoi(argv[2]);
+    vector<string> files = vector<string>();
+    vector<string> words;
+    vector<string> key;
+
+    getdir(dir,files);
+    string path = dir + "/" + files[0];
+    GetWords(path.c_str(), words);
+    convertToKey(words, key, N_WORDS);
+
+    return 0;
+}
 
 int getdir (string dir, vector<string> &files)
 {
@@ -28,80 +49,37 @@ int getdir (string dir, vector<string> &files)
 }
 
 
-void getletters(const char *fName, vector<char>& queue, int num){
+void GetWords(const char *fName, vector<string>& queue){
   string line;
   ifstream myfile (fName);
   if(myfile.is_open()){
     while(getline(myfile, line)){
       const char *letters = line.c_str();
+      string s;
       for(int i = 0; letters[i] != '\0'; i++){
-        if(('A' <= letters[i] && letters[i] <= 'Z') || letters[i] == ' ' ){
-          queue.push_back(letters[i]);
+        if ( ' ' == letters[i]){
+          queue.push_back(s);
+          s = "";
+        }
+        if(('A' <= letters[i] && letters[i] <= 'Z')){
+          s.push_back(letters[i]);
+
         }
         if('a' <= letters[i] && letters[i] <= 'z' ){
-          queue.push_back(letters[i] - 32);
+          s.push_back(letters[i] - 32);
         }
       }
-      queue.push_back(' ');
     }
   }
-  for (unsigned int i = 0;i < queue.size();i++) {
-      cout << queue[i];
-  }
-  cout << endl;
 }
 
-
-int main(int argc, char *argv[])
-{
-    string dir = argv[1];
-    const int N_WORDS = atoi(argv[2]);
-    vector<string> files = vector<string>();
-    vector<char> letters;
-    vector<string> words;
-    vector<string> key;
-
-    getdir(dir,files);
-
-    // for (unsigned int i = 0;i < files.size();i++) {
-    //     cout << i <<". " << files[i] << endl;
-    // }
-    string path = dir + "/" + files[0];
-    // cout << path<< endl;
-    getletters(path.c_str(), letters, 6);
-
-    // for (unsigned int i = 0;i < letters.size();i++) {
-    //     cout << letters[i];
-    // }
-    // cout << endl;
-    vector<char>::iterator it = letters.begin();
-    while(it != letters.end()){
-      if(*it == ' ' || *it == '\n'){
-        it++;
-      }
-      else{
-        string s;
-        while(*it != ' ' && *it != '\n'){
-          s = s + *it;
-          it++;
-        }
-        words.push_back(s);
-      }
+void convertToKey(vector<string>& queue, vector<string>& output, int num){
+  while (queue.size() >= num){
+    string s;
+    for(int i = 0; i < num; i++){
+      s = s + queue[i];
     }
-
-    for (unsigned int i = 0; i < words.size();i++) {
-        cout << i <<". " << words[i] << endl;
-    }
-    while (words.size() >= N_WORDS){
-      string s;
-      for(int i = 0; i < N_WORDS; i++){
-        s = s + words[i];
-      }
-      cout << s << endl;
-      words.erase(words.begin());
-      key.push_back(s);
-    }
-
-
-    return 0;
+    queue.erase(queue.begin());
+    output.push_back(s);
+  }
 }
